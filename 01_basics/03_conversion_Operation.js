0483fc42b9d393bf827a56c91c1e15ba10e039c2
rishabhsharma015 <60418660+rishabhsharma015@users.noreply.github.com>
2023-08-15 11:16:56 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversion_Operation.js
@@ -0,0 +1,38 @@
+let score = undefined;
+//console.log(typeof score);  
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber); 
+//console.log(valueInNumber)
+
+
+/*  NOTES
+    "33" => 33
+    "33abc" => NaN (Not a number)
+    true => 1 ; false => 0
+    null => 0 ; undefined => NaN
+*/
+
+
+///////////////////////////////////////////////////
+
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+/* NOTES
+    1 => true; 0 => false;
+    "abcd" => true (non-empty string); "" => false (empty string)
+*/
+
+//////////////////////////////////////////////////
+
+
+
+let num = 15;
+
+let str_num = String(num);
+console.log(str_num);
+console.log(typeof str_num);
