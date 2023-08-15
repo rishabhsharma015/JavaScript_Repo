c07b0ebdb920250f889ea6fae2f9c6c61c787729
rishabhsharma015 <60418660+rishabhsharma015@users.noreply.github.com>
2023-08-15 12:32:47 +0000

    done

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,22 @@
+// Datatypes
+/* 
+    1) Primitive types and their return types
+        number - number
+        string - string
+        boolean - boolean
+        bigint - bigint
+        null - object
+        undeifned - undefined
+        symbol - symbol
+
+    2) Non-Primitive
+        object - object
+        array - array
+        function - function
+*/
+
+let myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof myFunction)
