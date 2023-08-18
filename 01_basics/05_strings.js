d0dcb9b33f3d36925bea3de6ade4d01a0ac10db9
rishabhsharma015 <60418660+rishabhsharma015@users.noreply.github.com>
2023-08-18 18:03:40 +0000

    done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+// Declaring string
+const myName = "Rishabssh";
+let rollNo = 15;
+const name2 = new String('Rishabh');
+const role = "   sde   ";
+const bigString = "abc-def-ghi-jkl-mno-pqr-stu";
+
+// String Interpolation
+console.log(`My name is ${myName} and roll number is ${rollNo}`);
+
+// ------------------------String basic methods.---------------------------
+// console.log(myName.length);
+// console.log(myName.toUpperCase());
+// console.log(myName.toLowerCase());
+// console.log(myName.substring(0,4));
+// console.log(myName.slice(-5, -2));  // slice(startIdx, endIdx);
+// console.log(role.trim());
+// console.log(myName.replace('s', 'm'));
+// console.log(myName.replaceAll('s', 'm'));
+// console.log(myName.includes("i"));
+// console.log(bigString.split('-'));
+// console.log(myName.at(-3));
+// console.log(myName.startsWith('R'));
+// console.log(myName.endsWith('h'));
+// console.log(name2.valueOf());
+// console.log(myName.indexOf('s'));
+// console.log(myName.lastIndexOf('s'));
+// console.log(myName.concat("ABC"));
