b21d2ec87414d4ac07655e2554d7ebdd59d03049
rishabhsharma015 <60418660+rishabhsharma015@users.noreply.github.com>
2023-08-25 19:07:20 +0000

    done

---
+++ 06_dom/one.js
@@ -0,0 +1,6 @@
+// NodeList -> NodeList is an array-like object that has a collection of document nodes.(element nodes, attribute nodes and text nodes)
+//             NodeList object has forEach() method.
+//             NodeList is static.
+// HTMLCollection -> HTMLCollection is an array-like object that has a collection of document elements.(only element nodes)
+//                   HTMLCollection object don't have forEach() method.
+//                   HTMLCollection is live.
