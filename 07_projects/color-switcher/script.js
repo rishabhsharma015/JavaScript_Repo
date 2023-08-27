ea5df0b5150cec959ceff18f682bc83281df00be
rishabhsharma015 <60418660+rishabhsharma015@users.noreply.github.com>
2023-08-27 18:13:58 +0000

    done

--- 07_projects/color-switcher/script.js
+++ 07_projects/color-switcher/script.js
@@ -5,6 +5,13 @@ const body = document.querySelector("body");
 
 boxes.forEach( (box)=> {
     box.addEventListener('click', (event)=>{                   // 'event' is an Event object.
+
+        if(event.target.id == "grey" || event.target.id == "blue"){
+            body.style.color = "white";
+        }
+        if(event.target.id == "white" || event.target.id == "yellow"){
+            body.style.color = "black";
+        }
         body.style.backgroundColor = event.target.id;
     })
 })
